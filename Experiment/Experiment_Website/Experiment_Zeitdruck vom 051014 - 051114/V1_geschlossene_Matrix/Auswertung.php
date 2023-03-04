1828b1d72e095f2da7d8228860ad9c0813513a44
Marco Kremer <33638424+marcokremer@users.noreply.github.com>
2023-03-04 17:05:21 +0100

    Add files via upload

---
+++ Experiment/Experiment_Website/Experiment_Zeitdruck vom 051014 - 051114/V1_geschlossene_Matrix/Auswertung.php
@@ -0,0 +1,22 @@
+<?php 
+if (isset($_GET['subject'])) {$subject=$_GET['subject'];}
+ else {$subject="anonymous";}
+if (isset($_GET['condnum'])) {$condnum=$_GET['condnum'];}
+	else {$condnum=-1;}?><HTML>
+<HEAD>
+<TITLE>Auswertung Experiment-Serie V1_</TITLE>
+<script language=javascript src="mmlweb.js"></SCRIPT>
+<link rel="stylesheet" href="mlweb.css" type="text/css">
+</head>
+
+<body onLoad="timefunction('onload','body','body')">
+<CENTER><H1>Auswertung Experiment - Serie 2</H1></CENTER>
+<script language="javascript">
+ref_cur_hit = <?php echo($condnum);?>;
+subject = "<?php echo($subject);?>";
+</script>
+<P>
+<CENTER><TABLE border=1 width=80%>
+<TR>
+<TH> # </TH><TH>Spieler-ID</TH><TH>Ergebnis</TH></TR>
+
