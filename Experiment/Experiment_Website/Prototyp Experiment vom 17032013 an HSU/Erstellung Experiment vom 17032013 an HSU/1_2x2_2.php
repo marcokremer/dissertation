a1ced00ff706d02c60d7d4f3706f68ada875e8b5
Marco Kremer <33638424+marcokremer@users.noreply.github.com>
2023-03-04 17:04:10 +0100

    Add files via upload

---
+++ Experiment/Experiment_Website/Prototyp Experiment vom 17032013 an HSU/Erstellung Experiment vom 17032013 an HSU/1_2x2_2.php
@@ -0,0 +1,116 @@
+<?php 
+if (isset($_GET['subject'])) {$subject=$_GET['subject'];}
+ else {$subject="anonymous";}
+if (isset($_GET['condnum'])) {$condnum=$_GET['condnum'];}
+	else {$condnum=-1;}?><HTML>
+<HEAD>
+<TITLE>2Experimentneu</TITLE>
+<script language=javascript src="mmlweb.js"></SCRIPT>
+<link rel="stylesheet" href="mlweb.css" type="text/css">
+</head>
+
+<body onLoad="timefunction('onload','body','body')">
+<script language="javascript">
+ref_cur_hit = <?php echo($condnum);?>;
+subject = "<?php echo($subject);?>";
+</script>
+
+<!--BEGIN TABLE STRUCTURE-->
+<SCRIPT language="javascript">
+//override defaults
+mlweb_outtype="CSV";
+mlweb_fname="mlwebform";
+tag = "a0^a1`"
+ + "b0^b1";
+
+txt = "140;140^70;210`"
+ + "-1,1^1,-1";
+
+state = "0^0`"
+ + "0^0";
+
+box = "a0^a1`"
+ + "b0^b1";
+
+CBCol = "0^0";
+CBRow = "1^1";
+W_Col = "100^100";
+H_Row = "50^50";
+
+chkchoice = false;
+btnFlg = 2;
+btnType = "button";
+btntxt = "Strategie 1^Strategie 2";
+btnstate = "1^1";
+btntag = "btn1^btn2";
+to_email = "";
+colFix = false;
+rowFix = true;
+CBpreset = false;
+evtOpen = 1;
+evtClose = 1;
+chkFrm=false;
+warningTxt = "Some questions have not been answered. Please answer all questions before continuing!";
+tmTotalSec = 60;
+tmStepSec = 1;
+tmWidthPx = 200;
+tmFill = false;
+tmShowTime = true;
+tmCurTime = 0;
+tmActive = true;
+tmDirectStart = true;
+tmMinLabel = "min";
+tmSecLabel = "sec";
+tmLabel = "Timer: ";
+
+//Delay: a0 a1 b0 b1
+delay = "0^0^0^0`"
+ + "0^0^0^0`"
+ + "0^0^0^0`"
+ + "0^0^0^0";
+activeClass = "actTD";
+inactiveClass = "inactTD";
+boxClass = "boxTD";
+cssname = "mlweb.css";
+nextURL = "2_2x2_2.php";
+expname = "2strategien";
+randomOrder = true;
+recOpenCells = true;
+masterCond = 1;
+loadMatrices();
+</SCRIPT>
+<!--END TABLE STRUCTURE-->
+
+<FORM name="mlwebform" onSubmit="return checkForm(this)" method="POST" action="save.php"><INPUT type=hidden name="procdata" value="">
+<input type=hidden name="subject" value="">
+<input type=hidden name="expname" value="">
+<input type=hidden name="nextURL" value="">
+<input type=hidden name="choice" value="">
+<input type=hidden name="condnum" value="">
+<input type=hidden name="to_email" value="">
+<input type=hidden name="erwartung" value="">
+<!--BEGIN preHTML-->
+Wählen Sie eine Strategie per Mausklick auf den entsprechenden Knopf aus!
+
+
+<!--END preHTML-->
+<!-- MOUSELAB TABLE -->
+<TABLE border=1>
+<TR>
+<!--cell a0(tag:a0)-->
+<TD align=center valign=middle><DIV ID="a0_cont" style="position: relative; height: 50px; width: 100px;"><DIV ID="a0_txt" STYLE="position: absolute; left: 0px; top: 0px; height: 50px; width: 100px; clip: rect(0px 100px 50px 0px); z-index: 1;"><TABLE><TD ID="a0_td" align=center valign=center width=95 height=45 class="actTD">1,-1</TD></TABLE></DIV><DIV ID="a0_box" STYLE="position: absolute; left: 0px; top: 0px; height: 50px; width: 100px; clip: rect(0px 100px 50px 0px); z-index: 2;"><TABLE><TD ID="a0_tdbox" align=center valign=center width=95 height=45 class="boxTD">a0</TD></TABLE></DIV><DIV ID="a0_img" STYLE="position: absolute; left: 0px; top: 0px; height: 50px; width: 100px; z-index: 5;"><A HREF="javascript:void(0);" NAME="a0" onClick="SwitchCont('a0',event)"><IMG NAME="a0" SRC="transp.gif" border=0 width=100 height=50></A></DIV></DIV></TD>
+<!--end cell-->
+<!--cell a1(tag:a1)-->
+<TD align=center valign=middle><DIV ID="a1_cont" style="position: relative; height: 50px; width: 100px;"><DIV ID="a1_txt" STYLE="position: absolute; left: 0px; top: 0px; height: 50px; width: 100px; clip: rect(0px 100px 50px 0px); z-index: 1;"><TABLE><TD ID="a1_td" align=center valign=center width=95 height=45 class="actTD">-1,1</TD></TABLE></DIV><DIV ID="a1_box" STYLE="position: absolute; left: 0px; top: 0px; height: 50px; width: 100px; clip: rect(0px 100px 50px 0px); z-index: 2;"><TABLE><TD ID="a1_tdbox" align=center valign=center width=95 height=45 class="boxTD">a1</TD></TABLE></DIV><DIV ID="a1_img" STYLE="position: absolute; left: 0px; top: 0px; height: 50px; width: 100px; z-index: 5;"><A HREF="javascript:void(0);" NAME="a1" onClick="SwitchCont('a1',event)"><IMG NAME="a1" SRC="transp.gif" border=0 width=100 height=50></A></DIV></DIV></TD>
+<!--end cell--><TD ID="btn_0" style="border-left-style: none; border-right-style: none; border-bottom-style: none;" align=center valign=middle><INPUT type="button" class="btnStyle" name="btn1" value="Strategie 1" onMouseOver="timefunction('mouseover','btn1','Strategie 1')" onClick="recChoice('onclick','btn1','Strategie 1')" onMouseOut="timefunction('mouseout','btn1','Strategie 1')"></TD>
+</TR><TR>
+<!--cell b0(tag:b0)-->
+<TD align=center valign=middle><DIV ID="b0_cont" style="position: relative; height: 50px; width: 100px;"><DIV ID="b0_txt" STYLE="position: absolute; left: 0px; top: 0px; height: 50px; width: 100px; clip: rect(0px 100px 50px 0px); z-index: 1;"><TABLE><TD ID="b0_td" align=center valign=center width=95 height=45 class="actTD">-1,1</TD></TABLE></DIV><DIV ID="b0_box" STYLE="position: absolute; left: 0px; top: 0px; height: 50px; width: 100px; clip: rect(0px 100px 50px 0px); z-index: 2;"><TABLE><TD ID="b0_tdbox" align=center valign=center width=95 height=45 class="boxTD">b0</TD></TABLE></DIV><DIV ID="b0_img" STYLE="position: absolute; left: 0px; top: 0px; height: 50px; width: 100px; z-index: 5;"><A HREF="javascript:void(0);" NAME="b0" onClick="SwitchCont('b0',event)"><IMG NAME="b0" SRC="transp.gif" border=0 width=100 height=50></A></DIV></DIV></TD>
+<!--end cell-->
+<!--cell b1(tag:b1)-->
+<TD align=center valign=middle><DIV ID="b1_cont" style="position: relative; height: 50px; width: 100px;"><DIV ID="b1_txt" STYLE="position: absolute; left: 0px; top: 0px; height: 50px; width: 100px; clip: rect(0px 100px 50px 0px); z-index: 1;"><TABLE><TD ID="b1_td" align=center valign=center width=95 height=45 class="actTD">1,-1</TD></TABLE></DIV><DIV ID="b1_box" STYLE="position: absolute; left: 0px; top: 0px; height: 50px; width: 100px; clip: rect(0px 100px 50px 0px); z-index: 2;"><TABLE><TD ID="b1_tdbox" align=center valign=center width=95 height=45 class="boxTD">b1</TD></TABLE></DIV><DIV ID="b1_img" STYLE="position: absolute; left: 0px; top: 0px; height: 50px; width: 100px; z-index: 5;"><A HREF="javascript:void(0);" NAME="b1" onClick="SwitchCont('b1',event)"><IMG NAME="b1" SRC="transp.gif" border=0 width=100 height=50></A></DIV></DIV></TD>
+<!--end cell--><TD ID="btn_1" style="border-left-style: none; border-right-style: none; border-bottom-style: none;" align=center valign=middle><INPUT type="button" class="btnStyle" name="btn2" value="Strategie 2" onMouseOver="timefunction('mouseover','btn2','Strategie 2')" onClick="recChoice('onclick','btn2','Strategie 2')" onMouseOut="timefunction('mouseout','btn2','Strategie 2')"></TD>
+</TR><tr><td colspan=2><!-- Begin HTML Time Bar --><table><tr><td>Timer: </td><td colspan=2><div id="tmCont" class="tmCont"><div id="tmBar" class="tmBar"></div><div id="tmTime" class="tmTime"></div></div></td></TR></table><!-- End HTML Time Bar --></td></tr></TABLE>
+<!-- END MOUSELAB TABLE -->
+<!--BEGIN postHTML-->
+<!--END postHTML--><INPUT type="submit" value="nächste Seite" onClick=erwartungswert('submit','submit','submit')></FORM></body></html>
